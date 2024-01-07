54e4509f61393fb0a7443f3e43d743cb6e808cf6
uberphoebus <87922930+uberphoebus@users.noreply.github.com>
2024-01-07 04:07:18 +0000

    cpp standard library

---
+++ 02-6-1.cpp
@@ -0,0 +1,16 @@
+#include <cmath>   // math.h
+#include <cstdio>  // stdio.h
+#include <cstring> // string.h
+using namespace std;
+
+int main(void) {
+  char str1[] = "result";
+  char str2[30];
+
+  strcpy(str2, str1);
+
+  printf("%s : %f\n", str1, sin(0.14));
+  printf("%s : %f\n", str2, abs(-1.25));
+
+  return 0;
+}
