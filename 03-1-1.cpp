7f234ebdac51458ddc1fbf9a40946393bb03ad5c
uberphoebus <87922930+uberphoebus@users.noreply.github.com>
2024-01-07 04:42:00 +0000

    cpp method in struct

--- 03-1-1.cpp
+++ 03-1-1.cpp
@@ -11,52 +11,55 @@ struct Car {
   char gamerID[ID_LEN];
   int fuelGauge;
   int curSpeed;
-};
 
-void ShowCarState(const Car &car);
-void Accel(Car &car);
-void Break(Car &car);
+  // function in struct
+  void ShowCarState();
+  void Accel();
+  void Break();
+};
 
 int main(void) {
   // no typedef
   Car run99 = {"run99", 100, 0};
-  Accel(run99);
-  Accel(run99);
-  ShowCarState(run99);
-  Break(run99);
-  ShowCarState(run99);
+
+  // instance.method()
+  run99.Accel();
+  run99.Accel();
+  run99.ShowCarState();
+  run99.Break();
+  run99.ShowCarState();
 
   Car sped77 = {"sped77", 100, 0};
-  Accel(sped77);
-  Break(sped77);
-  ShowCarState(sped77);
+  sped77.Accel();
+  sped77.Break();
+  sped77.ShowCarState();
 
   return 0;
 }
 
-void ShowCarState(const Car &car) {
-  cout << "ID    : " << car.gamerID << endl;
-  cout << "fuel  : " << car.fuelGauge << " %" << endl;
-  cout << "speed : " << car.curSpeed << " km/s" << endl << endl;
+void Car::ShowCarState() {
+  cout << "ID    : " << gamerID << endl;
+  cout << "fuel  : " << fuelGauge << " %" << endl;
+  cout << "speed : " << curSpeed << " km/s" << endl << endl;
 }
-void Accel(Car &car) {
-  if (car.fuelGauge <= 0) {
+void Car::Accel() {
+  if (fuelGauge <= 0) {
     return;
   } else {
-    car.fuelGauge -= FUEL_STEP;
+    fuelGauge -= FUEL_STEP;
   }
 
-  if (car.curSpeed + ACC_STEP >= MAX_SPD) {
-    car.curSpeed = MAX_SPD;
+  if (curSpeed + ACC_STEP >= MAX_SPD) {
+    curSpeed = MAX_SPD;
     return;
   }
 
-  car.curSpeed += ACC_STEP;
+  curSpeed += ACC_STEP;
 }
-void Break(Car &car) {
-  if (car.curSpeed < BRK_STEP) {
-    car.curSpeed = 0;
+void Car::Break() {
+  if (curSpeed < BRK_STEP) {
+    curSpeed = 0;
     return;
   }
-  car.curSpeed -= BRK_STEP;
+  curSpeed -= BRK_STEP;
 }
